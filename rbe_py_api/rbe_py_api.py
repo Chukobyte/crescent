1d0d0edaa3733a5b9f9ae044620149d12b3776c8
chukobyte <chukobyte@gmail.com>
2022-06-07 08:00:35 -0400

    Working on project configuration.

--- rbe_py_api/rbe_py_api.py
+++ rbe_py_api/rbe_py_api.py
@@ -1,7 +1,52 @@
+class AudioSource:
+    def __init__(self, uid: str, file_path: str):
+        self.file_path = file_path
+
+
+class Texture:
+    def __init__(
+        self, file_path: str, wrap_s: str, wrap_t: str, filter_min: str, filter_mag: str
+    ):
+        self.file_path = file_path
+        self.wrap_s = wrap_s
+        self.wrap_t = wrap_t
+        self.filter_min = filter_min
+        self.filter_mag = filter_mag
+
+
+class Font:
+    def __init__(self, uid: str, file_path: str, size: int):
+        self.uid = uid
+        self.file_path = file_path
+        self.size = size
+
+
+class InputAction:
+    def __init__(self, name: str, values: list):
+        self.name = name
+        self.values = values
+
+
 def configure_game(
     game_tile="Test Game",
     window_width=800,
     window_height=600,
+    resolution_width=800,
+    resolution_height=800,
     target_fps=66,
 ):
     pass
+
+
+def create_assets(audio_sources=None, textures=None, fonts=None):
+    if fonts is None:
+        fonts = []
+    if textures is None:
+        textures = []
+    if audio_sources is None:
+        audio_sources = []
+
+
+def configure_inputs(input_actions=None):
+    if input_actions is None:
+        input_actions = []
--- test_games/fighter_test/rbe_config.py
+++ test_games/fighter_test/rbe_config.py
@@ -1,8 +1,47 @@
-from rbe_py_api import configure_game
+from rbe_py_api import (
+    configure_game,
+    AudioSource,
+    Texture,
+    Font,
+    InputAction,
+    create_assets,
+    configure_inputs,
+)
 
 configure_game(
-    game_tile='Fighter Test',
+    game_tile="Test Game",
     window_width=800,
     window_height=600,
     target_fps=66,
+)
+
+create_assets(
+    audio_sources=[
+        AudioSource(
+            file_path="test_games/fighter_test/assets/audio/sfx/rainbow_orb.wav"
+        ),
+    ],
+    textures=[
+        Texture(
+            file_path="test_games/fighter_test/assets/images/melissa_walk_animation.png",
+            wrap_s="clamp_to_border",
+            wrap_t="clamp_to_border",
+            filter_min="nearest",
+            filter_mag="nearest",
+        ),
+    ],
+    fonts=[
+        Font(
+            file_path="test_games/fighter_test/assets/fonts/verdana.ttf",
+            uid="verdana-32",
+            size=32,
+        )
+    ],
+)
+
+configure_inputs(
+    input_actions=[
+        InputAction(name="exit", values=["esc"]),
+        InputAction(name="play_sfx", values=["space"]),
+    ]
 )
