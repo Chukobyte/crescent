c581d04906052713ebcd7741df6797cbc0023555
chukobyte <chukobyte@gmail.com>
2022-06-06 22:58:22 -0400

    Clean.

--- engine/src/core/ecs/ecs_manager.h
+++ engine/src/core/ecs/ecs_manager.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// Top level class that helps simplify ecs initialization and finalization
+// Top level class that helps simplify ecs initialization and finalization.  Will most likely remove and place in core.c in the future.
 
 void rbe_ecs_manager_initialize();
 void rbe_ecs_manager_finalize();
