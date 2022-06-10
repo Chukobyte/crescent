42f07a507771f93f0f6c2c370ec66c18158a5e78
chukobyte <chukobyte@gmail.com>
2022-06-10 07:49:29 -0400

    Clean.

--- test_games/fighter_test/src/main.py
+++ test_games/fighter_test/src/main.py
@@ -7,7 +7,7 @@ class Main(Node2D):
         print(f"[SCRIPT] Entity entered stage with id = {self.entity_id}")
 
     def _start(self) -> None:
-        print(f"Entity entered stage with id = {self.entity_id}")
+        pass
 
     def _update(self, delta_time: float) -> None:
         pass
