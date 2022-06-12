426d9c3d48f65adb08473f59b4a9a4234d539a06
chukobyte <chukobyte@gmail.com>
2022-06-12 14:13:04 -0400

    Reworking script system.

--- engine/src/core/ecs/component/script_component.h
+++ engine/src/core/ecs/component/script_component.h
@@ -1,8 +1,15 @@
 #pragma once
 
+typedef enum ScriptContextType {
+    ScriptContextType_NONE = 0, // INVALID
+    ScriptContextType_NATIVE = 1 << 0, // C/C++
+    ScriptContextType_PYTHON = 1 << 1
+} ScriptContextType;
+
 typedef struct ScriptComponent {
     char* classPath;
     char* className;
+    ScriptContextType contextType;
 } ScriptComponent;
 
 ScriptComponent* script_component_create();
--- engine/src/core/ecs/system/script_ec_system.c
+++ engine/src/core/ecs/system/script_ec_system.c
@@ -3,9 +3,11 @@
 #include <Python.h>
 
 #include "ec_system.h"
-#include "../component/script_component.h"
-#include "../../utils/rbe_assert.h"
 #include "../../data_structures/rbe_hash_map.h"
+#include "../../scripting/python/py_script_context.h"
+#include "../../utils/rbe_assert.h"
+
+#define MAX_SCRIPT_CONTEXTS 4
 
 void script_system_on_entity_registered(Entity entity);
 void script_system_on_entity_unregistered(Entity entity);
@@ -15,15 +17,18 @@ void script_system_instance_physics_update(float deltaTime);
 EntitySystem* scriptSystem = NULL;
 RBEScriptContext* scriptContext = NULL;
 
-EntitySystem* script_ec_system_create(RBEScriptContext* newScriptContext) {
+//static RBEScriptContext* scriptContexts[MAX_SCRIPT_CONTEXTS];
+//static size_t scriptContextsCount;
+
+EntitySystem* script_ec_system_create() {
     RBE_ASSERT(scriptSystem == NULL);
     scriptSystem = rbe_ec_system_create();
     scriptSystem->on_entity_registered_func = script_system_on_entity_registered;
     scriptSystem->on_entity_unregistered_func = script_system_on_entity_unregistered;
     scriptSystem->process_func = script_system_instance_update;
     scriptSystem->physics_process_func = script_system_instance_physics_update;
-    RBE_ASSERT(newScriptContext != NULL);
-    scriptContext = newScriptContext;
+    scriptContext = rbe_py_create_script_context();
+    RBE_ASSERT(scriptContext != NULL);
     return scriptSystem;
 }
 
