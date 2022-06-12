ee3ce06fc51d32efc7f08e53e0a8b2d7e9df1716
chukobyte <chukobyte@gmail.com>
2022-06-11 20:44:54 -0400

    Clean.

--- engine/src/core/scripting/python/py_script_context.c
+++ engine/src/core/scripting/python/py_script_context.c
@@ -64,6 +64,7 @@ void py_on_update_all_instances(float deltaTime) {
         RBE_ASSERT_FMT(entitiesToUpdate[i] != NULL, "Python instance is null!");
         PyObject_CallMethod(entitiesToUpdate[i], "_update", "(f)", deltaTime);
     }
+    // TODO: More robust error checking
     PyErr_Print();
 }
 
