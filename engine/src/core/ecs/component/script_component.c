05d7450e1dd805fa1ec8d624b001764b3858a8bf
chukobyte <chukobyte@gmail.com>
2022-06-12 21:38:12 -0400

    Clean.

--- engine/src/core/ecs/component/script_component.c
+++ engine/src/core/ecs/component/script_component.c
@@ -6,7 +6,6 @@
 
 ScriptComponent* script_component_create() {
     ScriptComponent* scriptComponent = RBE_MEM_ALLOCATE(ScriptComponent);
-//    ScriptComponent* scriptComponent = RBE_MEM_ALLOCATE_SIZE(sizeof(ScriptComponent*) * 10);
     scriptComponent->classPath = NULL;
     scriptComponent->className = NULL;
     scriptComponent->contextType = ScriptContextType_NONE;
