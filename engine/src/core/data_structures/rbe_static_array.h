b2f955556963ea675fc90b7b0e75c2a9b52e5837
chukobyte <chukobyte@gmail.com>
2022-06-13 17:55:11 -0400

    Clean.

--- engine/src/core/data_structures/rbe_static_array.h
+++ engine/src/core/data_structures/rbe_static_array.h
@@ -10,7 +10,9 @@ static size_t ARRAY_NAME ##_count = 0
 ARRAY_NAME[ARRAY_NAME ##_count++] = ARRAY_VALUE
 
 #define RBE_STATIC_ARRAY_REMOVE(ARRAY_NAME, ARRAY_VALUE, EMPTY_VALUE) \
-for (size_t i = 0; i < ARRAY_NAME ##_count; i++) {       \
+{                                                                     \
+const size_t ARRAY_NAME ##_count_ref = ARRAY_NAME ##_count;\
+for (size_t i = 0; i < ARRAY_NAME ##_count_ref; i++) {       \
 if (ARRAY_NAME[i] == ARRAY_VALUE) {                      \
 ARRAY_NAME[i] = ARRAY_NAME[i + 1];                       \
 ARRAY_NAME[i + 1] = EMPTY_VALUE;                         \
@@ -19,5 +21,6 @@ ARRAY_NAME ##_count--;                                   \
 if (ARRAY_NAME[i] == EMPTY_VALUE) {                      \
 ARRAY_NAME[i] = ARRAY_NAME[i + 1];                       \
 ARRAY_NAME[i + 1] = EMPTY_VALUE;                         \
+}                                                        \
 }                                                        \
 }
