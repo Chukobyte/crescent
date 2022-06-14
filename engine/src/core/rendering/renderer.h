a99940d047d3e3309ea2931db2ee04860d166010
chukobyte <chukobyte@gmail.com>
2022-06-13 22:45:22 -0400

    More updates to font renderer.

--- engine/src/core/rendering/renderer.h
+++ engine/src/core/rendering/renderer.h
@@ -3,10 +3,11 @@
 #include <stdbool.h>
 
 #include "texture.h"
+#include "font.h"
 #include "../math/rbe_math.h"
 
 void rbe_renderer_initialize();
 void rbe_renderer_finalize();
 void rbe_renderer_queue_sprite_draw_call(Texture* texture, Rect2 sourceRect, Rect2 destRect, float rotation, Color color, bool flipX, bool flipY);
-void rbe_renderer_queue_font_batch();
+void rbe_renderer_queue_font_draw_call(Font* font, const char* text, float x, float y, float scale, Color color);
 void rbe_renderer_flush_batches();
