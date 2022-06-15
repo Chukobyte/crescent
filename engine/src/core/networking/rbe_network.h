eeccebae55152513af1df493f584f7063f27b464
chukobyte <chukobyte@gmail.com>
2022-06-14 23:00:48 -0400

    Clean.

--- engine/src/core/networking/rbe_network.h
+++ engine/src/core/networking/rbe_network.h
@@ -6,3 +6,8 @@
 bool rbe_udp_server_initialize(int port);
 bool rbe_udp_server_poll();
 void rbe_udp_server_finalize();
+
+//--- UDP CLIENT ---//
+bool rbe_udp_client_initialize(const char* serverAddr, int serverPort);
+bool rbe_udp_client_poll();
+void rbe_udp_client_finalize();
--- engine/src/main.c
+++ engine/src/main.c
@@ -4,6 +4,20 @@
 #include "core/networking/rbe_network.h"
 #include "core/utils/rbe_assert.h"
 
+#define RBE_TEST_PORT 8888
+
+void run_server() {
+    RBE_ASSERT(rbe_udp_server_initialize(RBE_TEST_PORT));
+    rbe_udp_server_poll();
+    rbe_udp_server_finalize();
+}
+
+void run_client() {
+    RBE_ASSERT(rbe_udp_client_initialize("127.0.0.1", RBE_TEST_PORT));
+    rbe_udp_client_poll();
+    rbe_udp_client_finalize();
+}
+
 int main(int argv, char** args) {
 //    if (!rbe_initialize(argv, args)) {
 //        return EXIT_FAILURE;
@@ -15,9 +29,8 @@ int main(int argv, char** args) {
 //
 //    rbe_shutdown();
 
-    RBE_ASSERT(rbe_udp_server_initialize(8888));
-    rbe_udp_server_poll();
-    rbe_udp_server_finalize();
+    run_server();
+//    run_client();
 
     return EXIT_SUCCESS;
 }
