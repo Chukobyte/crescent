278b2dae64fa90970586c3cdb29fd02871d55089
chukobyte <chukobyte@gmail.com>
2022-06-15 23:16:10 -0400

    Clean.

--- engine/src/core/networking/rbe_network.c
+++ engine/src/core/networking/rbe_network.c
@@ -195,36 +195,3 @@ void rbe_udp_client_finalize() {
     closesocket(client_socket);
     WSACleanup();
 }
-
-// Networking Testing Stuff
-
-//#define RBE_NETWORKING_PORT 8888
-//
-//// Server
-//void server_callback(const char* message) {
-//    printf("Received from client: '%s'\n", message);
-//    rbe_udp_server_send_message(message);
-//}
-//
-//void server_start() {
-//    rbe_udp_server_initialize(RBE_NETWORKING_PORT, server_callback);
-//    while (true) {}
-//    rbe_udp_server_finalize();
-//}
-//
-//// Client
-//void client_callback(const char* message) {
-//    printf("Received from server: '%s'\n", message);
-//}
-//
-//void client_start() {
-//    rbe_udp_client_initialize("127.0.0.1", RBE_NETWORKING_PORT, client_callback);
-//    char sendMessageBuffer[512];
-//    while (true) {
-//        memset(sendMessageBuffer,'\0', 512);
-//        printf("Enter message:\n");
-//        gets(sendMessageBuffer);
-//        rbe_udp_client_send_message(sendMessageBuffer);
-//    }
-//    rbe_udp_client_finalize();
-//}
