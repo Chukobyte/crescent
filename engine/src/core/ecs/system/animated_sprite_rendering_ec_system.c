3b6975538792fd14f7def4e2c024e94afe237a45
chukobyte <chukobyte@gmail.com>
2022-06-26 19:41:53 -0400

    Updating other ec systems transforms based on their parents.

--- engine/src/core/ecs/system/animated_sprite_rendering_ec_system.c
+++ engine/src/core/ecs/system/animated_sprite_rendering_ec_system.c
@@ -8,6 +8,7 @@
 #include "../component/transform2d_component.h"
 #include "../component/animated_sprite_component.h"
 #include "../../rendering/renderer.h"
+#include "../../scene/scene_manager.h"
 #include "../../utils/rbe_string_util.h"
 #include "../../utils/rbe_assert.h"
 
@@ -27,6 +28,7 @@ void animated_sprite_rendering_system_render() {
     const int currentTickTime = (int) SDL_GetTicks();
     for (size_t i = 0; i < animatedSpriteRenderingSystem->entity_count; i++) {
         const Entity entity = animatedSpriteRenderingSystem->entities[i];
+        const Transform2DComponent parentTransform = rbe_scene_manager_get_combined_parent_transform(entity);
         const Transform2DComponent* spriteTransformComp = (Transform2DComponent*) component_manager_get_component(entity, ComponentDataIndex_TRANSFORM_2D);
         AnimatedSpriteComponent* animatedSpriteComponent = (AnimatedSpriteComponent*) component_manager_get_component(entity, ComponentDataIndex_ANIMATED_SPRITE);
         AnimationFrame currentFrame = animatedSpriteComponent->currentAnimation.animationFrames[animatedSpriteComponent->currentAnimation.currentFrame];
@@ -45,10 +47,10 @@ void animated_sprite_rendering_system_render() {
             }
         }
         const Rect2 destinationRectangle = {
-            spriteTransformComp->position.x,
-            spriteTransformComp->position.y,
-            currentFrame.drawSource.w * spriteTransformComp->scale.x,
-            currentFrame.drawSource.h * spriteTransformComp->scale.y
+            spriteTransformComp->position.x + parentTransform.position.x,
+            spriteTransformComp->position.y + parentTransform.position.y,
+            currentFrame.drawSource.w * spriteTransformComp->scale.x * parentTransform.scale.x,
+            currentFrame.drawSource.h * spriteTransformComp->scale.y * parentTransform.scale.y
         };
         rbe_renderer_queue_sprite_draw_call(
             currentFrame.texture,
--- engine/src/core/ecs/system/font_rendering_ec_system.c
+++ engine/src/core/ecs/system/font_rendering_ec_system.c
@@ -4,6 +4,7 @@
 #include "../component/transform2d_component.h"
 #include "../component/text_label_component.h"
 #include "../../rendering/renderer.h"
+#include "../../scene/scene_manager.h"
 #include "../../utils/rbe_string_util.h"
 #include "../../utils/rbe_assert.h"
 
@@ -24,14 +25,15 @@ EntitySystem* font_rendering_ec_system_create() {
 void font_rendering_system_render() {
     for (size_t i = 0; i < fontRenderingSystem->entity_count; i++) {
         const Entity entity = fontRenderingSystem->entities[i];
+        const Transform2DComponent parentTransform = rbe_scene_manager_get_combined_parent_transform(entity);
         Transform2DComponent* fontTransformComp = (Transform2DComponent*) component_manager_get_component(entity, ComponentDataIndex_TRANSFORM_2D);
         TextLabelComponent* textLabelComponent = (TextLabelComponent*) component_manager_get_component(entity, ComponentDataIndex_TEXT_LABEL);
         rbe_renderer_queue_font_draw_call(
             textLabelComponent->font,
             textLabelComponent->text,
-            fontTransformComp->position.x,
-            fontTransformComp->position.y,
-            fontTransformComp->scale.x,
+            fontTransformComp->position.x + parentTransform.position.x,
+            fontTransformComp->position.y + parentTransform.position.y,
+            fontTransformComp->scale.x * parentTransform.scale.x,
             textLabelComponent->color
         );
     }
