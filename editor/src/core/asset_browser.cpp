7ba593c18d2058c4980a4bbace21e0e44f2a932b
chukobyte <chukobyte@gmail.com>
2022-09-03 19:42:48 -0400

    Clean.

--- editor/src/core/asset_browser.cpp
+++ editor/src/core/asset_browser.cpp
@@ -4,13 +4,27 @@
 #include "utils/file_system_helper.h"
 #include "../../../engine/src/core/utils/logger.h"
 
-#include <filesystem>
-
 using namespace Squid;
+
+void FileNodeUtils::LoadFileNodeDirEntries(FileNode& fileNode) {
+    for (auto const& dir_entry : std::filesystem::directory_iterator{fileNode.path}) {
+//        rbe_logger_debug("dir entry path: '%s'", dir_entry.path().string().c_str());
+        rbe_logger_debug("dir entry relative path: '%s'", std::filesystem::relative(dir_entry.path(), fileNode.path).string().c_str());
+        if (std::filesystem::is_directory(dir_entry)) {
+            FileNode dirNode = { dir_entry.path(), FileNodeType::Directory };
+            LoadFileNodeDirEntries(dirNode);
+            fileNode.directories.emplace_back(dirNode);
+        } else if (std::filesystem::is_regular_file(dir_entry)) {
+            FileNode regularFileNode = { dir_entry.path(), FileNodeType::File };
+            fileNode.files.emplace_back(regularFileNode);
+        }
+    }
+}
 
 Task<> AssetBrowser::UpdateFileSystemCache() {
     while (true) {
-        co_await WaitSeconds(10.0f, EditorContext::Time);
+//        co_await WaitSeconds(10.0f, EditorContext::Time);
+        co_await WaitSeconds(3.0f, EditorContext::Time);
 
         RefreshCache();
     }
@@ -21,7 +35,7 @@ void AssetBrowser::RefreshCache() {
     rbe_logger_debug("root name: %s", projectRootDir.root_name().c_str());
     rbe_logger_debug("root path: %s", projectRootDir.string().c_str());
 
-    for (auto const& dir_entry : std::filesystem::directory_iterator{projectRootDir}) {
-        rbe_logger_debug("dir entry path: '%s'", dir_entry.path().string().c_str());
-    }
+    rootNode.path = projectRootDir;
+    rootNode.type = FileNodeType::Directory;
+    FileNodeUtils::LoadFileNodeDirEntries(rootNode);
 }
--- editor/src/core/asset_browser.h
+++ editor/src/core/asset_browser.h
@@ -2,15 +2,36 @@
 
 #include "utils/singleton.h"
 
+#include <vector>
+#include <filesystem>
+
 #include "SquidTasks/Task.h"
 
+// A wrapper around filesystem path
+enum class FileNodeType {
+    Invalid,
+    File,
+    Directory,
+};
 
+struct FileNode {
+    std::filesystem::path path;
+    FileNodeType type = FileNodeType::Invalid;
+    std::vector<FileNode> directories;
+    std::vector<FileNode> files;
+};
+
+namespace FileNodeUtils {
+void LoadFileNodeDirEntries(FileNode& fileNode);
+}
 
 class AssetBrowser : public Singleton<AssetBrowser> {
   public:
     AssetBrowser(singleton) {}
 
     Task<> UpdateFileSystemCache();
+
+    FileNode rootNode;
 
   private:
     void RefreshCache();
