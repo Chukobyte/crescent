b8b24fd7e6a0ae68f1451c35069d55a7f4add8de
chukobyte <chukobyte@gmail.com>
2022-09-05 22:18:43 -0400

    More scene file creation.

--- editor/src/core/file_creation/scene_file_creator.cpp
+++ editor/src/core/file_creation/scene_file_creator.cpp
@@ -5,6 +5,22 @@
 
 void SceneFileCreator::GenerateSceneFile(SceneNodeFile *nodeFile, const char *filePath) {
     std::string fileContents = "from crescent_api import *\n";
+    fileContents += "\n";
+    // Begin
+    fileContents += "create_stage_nodes(\n";
+    fileContents += "    stage_nodes=[\n";
+    // Stage Nodes
+    fileContents += "        StageNode(\n";
+    fileContents += "            name=\"Main\",\n";
+    fileContents += "            type=\"Node2D\",\n";
+    fileContents += "            tags=None,\n";
+    fileContents += "            external_node_source=None,\n";
+    fileContents += "            components=[],\n";
+    fileContents += "            children=[],\n";
+    fileContents += "        ),\n";
+    // End
+    fileContents += "    ],\n";
+    fileContents += ")\n";
 
     FileSystemHelper::WriteFile(filePath, fileContents);
 }
