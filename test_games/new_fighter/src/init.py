d507e2f7fb0a5bdd05e4e6418191bf6602971152
chukobyte <chukobyte@gmail.com>
2022-09-24 22:44:41 -0400

    Clean.

--- test_games/new_fighter/src/init.py
+++ test_games/new_fighter/src/init.py
@@ -3,4 +3,4 @@ from crescent_api import *
 
 class Init(Node2D):
     def _start(self) -> None:
-        SceneTree.change_scene(path="nodes/title_screen_node.py")
+        SceneTree.change_scene(path="nodes/title_screen_node.cscn")
