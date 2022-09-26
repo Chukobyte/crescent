a0c3f5e3a467204d544e51f2376d457cf0c97237
chukobyte <chukobyte@gmail.com>
2022-09-26 06:55:24 -0400

    Setting up new game project.

--- test_games/new_fighter/src/title_screen.py
+++ test_games/new_fighter/src/title_screen.py
@@ -3,5 +3,5 @@ from crescent_api import *
 
 class TitleScreen(Node2D):
     def _update(self, delta_time: float) -> None:
-        if Input.is_action_just_pressed(name="quit"):
+        if Input.is_action_just_pressed(name="exit"):
             Engine.exit()
