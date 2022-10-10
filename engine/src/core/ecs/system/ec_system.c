95525721dc3c766309245dc8c10f54a18a2838d9
chukobyte <chukobyte@gmail.com>
2022-10-10 12:24:33 -0400

    Clean.

--- engine/src/core/ecs/system/ec_system.c
+++ engine/src/core/ecs/system/ec_system.c
@@ -158,7 +158,6 @@ void rbe_ec_system_network_callback(const char* message) {
 Entity rbe_ec_system_create_entity() {
     // TODO: Create entity pool
     Entity newEntity = entityIndex;
-//    rbe_logger_info("New entity created with id = '%d'", newEntity);
     entityIndex++;
     return newEntity;
 }
