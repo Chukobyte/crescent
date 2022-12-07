52393889a6bab0eb80c1746bdd36521fdac0f67a
chukobyte <chukobyte@gmail.com>
2022-12-06 21:26:52 -0500

    Adding inputs and test stuff.

--- test_games/dec_jam/src/player.py
+++ test_games/dec_jam/src/player.py
@@ -2,5 +2,16 @@ from crescent_api import *
 
 
 class Player(Node2D):
+    def __init__(self, entity_id: int):
+        super().__init__(entity_id)
+        self.color_rect = None
+        self.collider = None
+
     def _start(self) -> None:
+        self.color_rect = self.get_child("ColorRect")
+        self.collider = self.get_child("Collider2D")
         print("Player Start")
+
+    def _update(self, delta_time: float) -> None:
+        if Input.is_action_just_pressed(name="quit_game"):
+            Engine.exit()
