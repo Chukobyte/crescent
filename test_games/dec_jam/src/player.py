ec5b2e2c67679f30b7b3e55c4e42b7f37ea05cd5
chukobyte <chukobyte@gmail.com>
2022-12-06 21:38:13 -0500

    Rough movement.

--- test_games/dec_jam/src/player.py
+++ test_games/dec_jam/src/player.py
@@ -6,6 +6,7 @@ class Player(Node2D):
         super().__init__(entity_id)
         self.color_rect = None
         self.collider = None
+        self.speed = 200
 
     def _start(self) -> None:
         self.color_rect = self.get_child("ColorRect")
@@ -15,3 +16,9 @@ class Player(Node2D):
     def _update(self, delta_time: float) -> None:
         if Input.is_action_just_pressed(name="quit_game"):
             Engine.exit()
+
+    def _physics_update(self, delta_time: float) -> None:
+        if Input.is_action_pressed(name="move_left"):
+            self.add_to_position(Vector2.LEFT() * Vector2(delta_time * self.speed, delta_time * self.speed))
+        elif Input.is_action_pressed(name="move_right"):
+            self.add_to_position(Vector2.RIGHT() * Vector2(delta_time * self.speed, delta_time * self.speed))
