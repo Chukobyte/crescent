2719dad1012480909132d3047068c84e486a919a
chukobyte <chukobyte@gmail.com>
2022-12-17 14:05:31 -0500

    Prototyping...

--- test_games/dec_jam/src/enemy.py
+++ test_games/dec_jam/src/enemy.py
@@ -2,12 +2,7 @@ from crescent_api import Node2D, ColorRect, Collider2D, Size2D, Color
 
 
 class Enemy(Node2D):
-    pass
-
-
-class GingerBreadMan(Enemy):
-    def _start(self) -> None:
-        size = Size2D(48, 64)
+    def _init_components(self, size: Size2D) -> None:
         color_rect = ColorRect.new()
         color_rect.size = size
         color_rect.color = Color.linear_color(0.9, 0.9, 0.9)
@@ -15,3 +10,8 @@ class GingerBreadMan(Enemy):
         collider_2d = Collider2D.new()
         collider_2d.extents = size
         self.add_child(collider_2d)
+
+
+class GingerBreadMan(Enemy):
+    def _start(self) -> None:
+        self._init_components(size=Size2D(48, 64))
