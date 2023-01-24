c1ec60f636b67f160e13a82aa57a98741ea81506
chukobyte <chukobyte@gmail.com>
2023-01-24 07:16:57 -0500

    Attempting to fix editor test link issues.

--- editor/test/main.cpp
+++ editor/test/main.cpp
@@ -1,2 +1,10 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+
+#include <SDL2/SDL_main.h>
+
+int main(int argc, char** argv) {
+    int result = Catch::Session().run( argc, argv );
+
+    return result;
+}
