136d2701beffc90eafc9bfd919a64e99c2377e13
Chukobyte <chukobyte@gmail.com>
2023-02-13 15:04:45 -0500

    Update packaging for linux and mac python deps.

--- ci/download_package_deps.py
+++ ci/download_package_deps.py
@@ -10,28 +10,35 @@ WINDOWS_ENGINE_EDITOR_BINARY = "crescent_engine_editor_windows.exe"
 WINDOWS_RUN_DEPS_ZIP = "windows_run_dependencies.zip"
 LINUX_ENGINE_BINARY = "crescent_engine_linux"
 LINUX_ENGINE_EDITOR_BINARY = "crescent_engine_editor_linux"
-LINUX_PYTHON_ZIP = f"python-{PYTHON_VERSION}-embed-macosx.zip"
+LINUX_PYTHON_ZIP = f"python-{PYTHON_VERSION}-embed-linux.zip"
 MACOSX_ENGINE_BINARY = "crescent_engine_macosx"
 MACOSX_PYTHON_ZIP = f"python-{PYTHON_VERSION}-embed-macosx.zip"
 
-ALL_DEP_FILES = [
+BINARY_FILES = [
     WINDOWS_ENGINE_BINARY,
     WINDOWS_ENGINE_EDITOR_BINARY,
-    WINDOWS_RUN_DEPS_ZIP,
     LINUX_ENGINE_BINARY,
     LINUX_ENGINE_EDITOR_BINARY,
-    # LINUX_PYTHON_ZIP,
     MACOSX_ENGINE_BINARY,
-    # MACOSX_PYTHON_ZIP,
 ]
+
+DEP_FILES = [
+    WINDOWS_RUN_DEPS_ZIP,
+    LINUX_PYTHON_ZIP,
+    MACOSX_PYTHON_ZIP,
+]
+
+ALL_FILES = BINARY_FILES + DEP_FILES
 
 if len(sys.argv) >= 2:
     cred_file_path = sys.argv[1]
     drive_service = GoogleDriveService(credential_file_path=cred_file_path)
-    print(f"Beginning to download the following dependencies: {ALL_DEP_FILES}")
-    drive_service.download_files(ALL_DEP_FILES)
+    print(f"Downloading the following binaries: {BINARY_FILES}")
+    drive_service.download_files(BINARY_FILES)
+    print(f"Downloading the dependency files: {DEP_FILES}")
+    drive_service.download_files(DEP_FILES)
     files_not_downloaded = []
-    for binary_path in ALL_DEP_FILES:
+    for binary_path in ALL_FILES:
         if not pathlib.Path(binary_path).exists():
             files_not_downloaded.append(binary_path)
     if files_not_downloaded:
