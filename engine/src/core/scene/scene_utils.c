879c8b092fe774c4b9b8d743586433f1126bf1d0
chukobyte <chukobyte@gmail.com>
2023-03-15 09:01:30 -0400

    Clean.

--- engine/src/core/scene/scene_utils.c
+++ engine/src/core/scene/scene_utils.c
@@ -12,8 +12,7 @@ on_get_local_transform onGetLocalTransformFunc = &default_get_local_transform;
 
 // Default engine callbacks
 SETransform2D default_get_local_transform(CreEntity entity, int* zIndex, bool* success) {
-    Transform2DComponent* transform2DComponent = cre_component_manager_get_component_unchecked(entity,
-            CreComponentDataIndex_TRANSFORM_2D);
+    Transform2DComponent* transform2DComponent = cre_component_manager_get_component_unchecked(entity, CreComponentDataIndex_TRANSFORM_2D);
     if (transform2DComponent == NULL) {
         *success = false;
         return (SETransform2D) {
