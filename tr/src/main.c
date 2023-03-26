693c33bfe7f56a253aa1a14b988c0ebae2660f45
chukobyte <chukobyte@gmail.com>
2023-03-26 10:29:15 -0400

    Starting on test renderer.

---
+++ tr/src/main.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "tr_renderer.h"
+
+int main(int argv, char** args) {
+    test_thing();
+    return EXIT_SUCCESS;
+}
---
+++ tr/src/tr_renderer.c
@@ -0,0 +1,7 @@
+#include "tr_renderer.h"
+
+#include <stdio.h>
+
+void test_thing() {
+    printf("Test");
+}
