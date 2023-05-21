96eb41547822b5b74c5f41f0ea9c4ef40f9a0259
chukobyte <chukobyte@gmail.com>
2023-05-20 21:38:48 -0400

    Added ensure as a non-fatal assert.

--- seika/src/utils/se_assert.h
+++ seika/src/utils/se_assert.h
@@ -30,6 +30,49 @@ exit(SE_ASSERT_FAIL_EXIT_CODE);                                                 
 }                                                                                     \
 )
 
+// Non-fatal asserts
+#define SE_ENSURE(PREDICATE)                                                         \
+SE_DEBUG_MACRO(                                                                      \
+if (!(PREDICATE)) {                                                                  \
+static bool hasEnsureTriggered = false;                                              \
+if (!hasEnsureTriggered) {                                                           \
+se_logger_print_err("Ensure failed in file '%s' on line %u!\n", __FILE__, __LINE__); \
+if (se_platform_is_debugger_attached()) { SE_TRIGGER_BREAKPOINT; }                   \
+hasEnsureTriggered = true;                                                           \
+}                                                                                    \
+}                                                                                    \
+)
+
+#define SE_ENSURE_FMT(PREDICATE, FMT, ...)                                         \
+SE_DEBUG_MACRO(                                                                    \
+if (!(PREDICATE)) {                                                                \
+static bool hasEnsureTriggered = false;                                            \
+if (!hasEnsureTriggered) {                                                         \
+se_logger_print_err("Ensure failed in file '%s' on line %u!", __FILE__, __LINE__); \
+se_logger_print_err(FMT, ##__VA_ARGS__);                                           \
+if (se_platform_is_debugger_attached()) { SE_TRIGGER_BREAKPOINT; }                 \
+hasEnsureTriggered = true;                                                         \
+}                                                                                  \
+}                                                                                  \
+)
+
+#define SE_ENSURE_ALWAYS(PREDICATE)                                                  \
+SE_DEBUG_MACRO(                                                                      \
+if (!(PREDICATE)) {                                                                  \
+se_logger_print_err("Ensure failed in file '%s' on line %u!\n", __FILE__, __LINE__); \
+if (se_platform_is_debugger_attached()) { SE_TRIGGER_BREAKPOINT; }                   \
+}                                                                                    \
+)
+
+#define SE_ENSURE_ALWAYS_FMT(PREDICATE, FMT, ...)                                  \
+SE_DEBUG_MACRO(                                                                    \
+if (!(PREDICATE)) {                                                                \
+se_logger_print_err("Ensure failed in file '%s' on line %u!", __FILE__, __LINE__); \
+se_logger_print_err(FMT, ##__VA_ARGS__);                                           \
+if (se_platform_is_debugger_attached()) { SE_TRIGGER_BREAKPOINT; }                 \
+}                                                                                  \
+)
+
 #ifdef __cplusplus
 }
 #endif
