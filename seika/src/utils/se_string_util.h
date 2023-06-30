b68b4d07e99402aa7d880a56c2c74edbd6e810f4
chukobyte <chukobyte@gmail.com>
2023-06-30 08:38:57 -0400

    Clean.

--- seika/src/utils/se_string_util.h
+++ seika/src/utils/se_string_util.h
@@ -14,5 +14,5 @@ char* se_str_to_lower_and_underscore_whitespace(char* str);
 char* se_str_trim(const char* value, char delimiter);
 // Same as 'se_str_trim' but will replace the 'delimiter' with the 'replacementValue'.
 char* se_str_trim_and_replace(const char* value, char delimiter, const char* replacementValue);
-// Removes instances of '\r' from a string
+// Removes all instances of the passed in char from the string
 void se_str_remove_char(char* string, char charToRemove);
