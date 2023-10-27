027b80c6ed9707b7714a295dbbe3eab0ee02828b
chukobyte <chukobyte@gmail.com>
2023-10-27 00:23:12 -0400

    More tests.

--- engine/src/core/scripting/python/pocketpy/cre_py_api.c
+++ engine/src/core/scripting/python/pocketpy/cre_py_api.c
@@ -5,12 +5,28 @@
 #include "../../../ecs/entity/entity.h"
 #include "../../../ecs/component/node_component.h"
 #include "../../../ecs/component/component.h"
+#include "../../../scene/scene_utils.h"
 
 int cre_py_pp_api_node_get_name(pkpy_vm* vm) {
     int entityId;
     pkpy_to_int(vm, 0, &entityId);
+
     const CreEntity entity = (CreEntity)entityId;
     NodeComponent* nodeComponent = (NodeComponent*) cre_component_manager_get_component(entity, CreComponentDataIndex_NODE);
     pkpy_push_string(vm, pkpy_string(nodeComponent->name));
     return 1;
+}
+
+int cre_py_pp_api_node_get_children(pkpy_vm* vm) {
+    int entityId;
+    pkpy_to_int(vm, 0, &entityId);
+
+    const CreEntity entity = (CreEntity)entityId;
+    const SceneTreeNode* parentTreeNode = cre_scene_manager_get_entity_tree_node(entity);
+    for (size_t i = 0; i < parentTreeNode->childCount; i++) {
+        const SceneTreeNode* childTreeNode = parentTreeNode->children[i];
+        pkpy_push_int(vm, (int)childTreeNode->entity);
+    }
+
+    return (int)parentTreeNode->childCount;
 }
--- engine/src/core/scripting/python/pocketpy/cre_py_api.h
+++ engine/src/core/scripting/python/pocketpy/cre_py_api.h
@@ -3,3 +3,4 @@
 #include "pocketpy_c.h"
 
 int cre_py_pp_api_node_get_name(pkpy_vm* vm);
+int cre_py_pp_api_node_get_children(pkpy_vm* vm);
