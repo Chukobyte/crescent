0b4988c1dd96c271f024c8a370ed708d43f43f5e
chukobyte <chukobyte@gmail.com>
2023-11-22 22:02:02 -0500

    Clean.

--- engine/test/resources/test_custom_nodes.py
+++ engine/test/resources/test_custom_nodes.py
@@ -6,6 +6,9 @@ class TestNode(Node):
     def _start(self) -> None:
         print(f"Start called on {self}")
 
+    def _end(self) -> None:
+        print(f"End called on {self}")
+
     @staticmethod
     def new() -> "TestNode":
         return crescent_internal.node_new("test_custom_nodes", "TestNode", NodeType.Node)
