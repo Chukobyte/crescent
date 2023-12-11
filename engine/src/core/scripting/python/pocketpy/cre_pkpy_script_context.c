c43763f88bf33a6889304903e312f7577367efa4
Chukobyte <michaelsmith8293@gmail.com>
2023-12-10 23:18:19 -0500

    Clean up.

--- engine/src/core/scripting/python/pocketpy/cre_pkpy_script_context.c
+++ engine/src/core/scripting/python/pocketpy/cre_pkpy_script_context.c
@@ -44,7 +44,7 @@ SEObserver animated_sprite_frame_changed_observer = { .on_notify = pkpy_node_eve
 SEObserver animated_sprite_animation_finished_observer = { .on_notify = pkpy_node_event_animated_sprite_animation_finished };
 
 CREScriptContext* pkpy_script_context = NULL;
-pkpy_vm* vm = NULL;
+pkpy_vm* cre_pkpy_vm = NULL;
 
 pkpy_CName pyStartFunctionName;
 pkpy_CName pyUpdateFunctionName;
@@ -67,17 +67,17 @@ CREScriptContext* cre_pkpy_script_context_create() {
 
     pkpy_script_context = scriptContext;
 
-    if (!vm) {
-        vm = pkpy_new_vm(false);
+    if (!cre_pkpy_vm) {
+        cre_pkpy_vm = pkpy_new_vm(false);
         pyStartFunctionName = pkpy_name("_start");
         pyUpdateFunctionName = pkpy_name("_process");
         pyFixedUpdateFunctionName = pkpy_name("_fixed_process");
         pyEndFunctionName = pkpy_name("_end");
     }
 
-    cre_pkpy_api_load_internal_modules(vm);
-    cre_pkpy_node_event_manager_initialize(vm);
-    cre_pkpy_entity_instance_cache_initialize(vm);
+    cre_pkpy_api_load_internal_modules(cre_pkpy_vm);
+    cre_pkpy_node_event_manager_initialize(cre_pkpy_vm);
+    cre_pkpy_entity_instance_cache_initialize(cre_pkpy_vm);
 
     return scriptContext;
 }
@@ -104,15 +104,15 @@ void cre_pkpy_script_context_setup_node_event(CreEntity entity) {
 }
 
 void cre_pkpy_script_context_create_instance(CreEntity entity, const char* classPath, const char* className) {
-    SE_ASSERT(vm);
-    cre_pkpy_entity_instance_cache_create_new_entity(vm, classPath, className, entity);
+    SE_ASSERT(cre_pkpy_vm);
+    cre_pkpy_entity_instance_cache_create_new_entity(cre_pkpy_vm, classPath, className, entity);
     cre_pkpy_script_context_setup_node_event(entity);
 }
 
 void cre_pkpy_script_context_create_instance_if_nonexistent(CreEntity entity, const char* classPath, const char* className) {
-    SE_ASSERT(vm);
+    SE_ASSERT(cre_pkpy_vm);
     // If exists, just make sure node events are setup
-    if (cre_pkpy_entity_instance_cache_has_entity(vm, entity)) {
+    if (cre_pkpy_entity_instance_cache_has_entity(cre_pkpy_vm, entity)) {
         cre_pkpy_script_context_setup_node_event(entity);
     } else {
         cre_pkpy_script_context_create_instance(entity, classPath, className);
@@ -120,7 +120,7 @@ void cre_pkpy_script_context_create_instance_if_nonexistent(CreEntity entity, co
 }
 
 void cre_pkpy_script_context_create_instance_if_nonexistent_and_push_entity_instance(CreEntity entity) {
-    SE_ASSERT(vm);
+    SE_ASSERT(cre_pkpy_vm);
     const NodeComponent* nodeComponent = (NodeComponent*) cre_component_manager_get_component_unchecked(entity, CreComponentDataIndex_NODE);
     ScriptComponent* scriptComponent = (ScriptComponent*) cre_component_manager_get_component_unchecked(entity, CreComponentDataIndex_SCRIPT);
     if (!scriptComponent) {
@@ -132,68 +132,68 @@ void cre_pkpy_script_context_create_instance_if_nonexistent_and_push_entity_inst
         cre_component_manager_set_component(entity, CreComponentDataIndex_SCRIPT, scriptComponent);
     }
     cre_pkpy_script_context_create_instance_if_nonexistent(entity, scriptComponent->classPath, scriptComponent->className);
-    cre_pkpy_entity_instance_cache_push_entity_instance(vm, entity);
+    cre_pkpy_entity_instance_cache_push_entity_instance(cre_pkpy_vm, entity);
 }
 
 void pkpy_sc_on_delete_instance(CreEntity entity) {
-    SE_ASSERT(vm);
-    cre_pkpy_entity_instance_cache_remove_entity(vm, entity);
+    SE_ASSERT(cre_pkpy_vm);
+    cre_pkpy_entity_instance_cache_remove_entity(cre_pkpy_vm, entity);
 
     entityInitializedList[entity] = false;
 }
 
 void pkpy_sc_on_start(CreEntity entity) {
-    SE_ASSERT(vm);
-    cre_pkpy_entity_instance_cache_push_entity_instance(vm, entity);
-    if (pkpy_getattr(vm, pyStartFunctionName)) {
-        pkpy_push_null(vm);
-        pkpy_vectorcall(vm, 0);
+    SE_ASSERT(cre_pkpy_vm);
+    cre_pkpy_entity_instance_cache_push_entity_instance(cre_pkpy_vm, entity);
+    if (pkpy_getattr(cre_pkpy_vm, pyStartFunctionName)) {
+        pkpy_push_null(cre_pkpy_vm);
+        pkpy_vectorcall(cre_pkpy_vm, 0);
     }
-    pkpy_pop_top(vm);
+    pkpy_pop_top(cre_pkpy_vm);
 }
 
 void pkpy_sc_on_update_instance(CreEntity entity, float deltaTime) {
-    SE_ASSERT(vm);
-    cre_pkpy_entity_instance_cache_push_entity_instance(vm, entity);
-    if (pkpy_getattr(vm, pyUpdateFunctionName)) {
-        pkpy_push_null(vm);
-        pkpy_vectorcall(vm, 0);
+    SE_ASSERT(cre_pkpy_vm);
+    cre_pkpy_entity_instance_cache_push_entity_instance(cre_pkpy_vm, entity);
+    if (pkpy_getattr(cre_pkpy_vm, pyUpdateFunctionName)) {
+        pkpy_push_null(cre_pkpy_vm);
+        pkpy_vectorcall(cre_pkpy_vm, 0);
     }
-    pkpy_pop_top(vm);
+    pkpy_pop_top(cre_pkpy_vm);
 }
 
 void pkpy_sc_on_fixed_update_instance(CreEntity entity, float deltaTime) {
-    SE_ASSERT(vm);
-    cre_pkpy_entity_instance_cache_push_entity_instance(vm, entity);
-    if (pkpy_getattr(vm, pyFixedUpdateFunctionName)) {
-        pkpy_push_null(vm);
-        pkpy_vectorcall(vm, 0);
+    SE_ASSERT(cre_pkpy_vm);
+    cre_pkpy_entity_instance_cache_push_entity_instance(cre_pkpy_vm, entity);
+    if (pkpy_getattr(cre_pkpy_vm, pyFixedUpdateFunctionName)) {
+        pkpy_push_null(cre_pkpy_vm);
+        pkpy_vectorcall(cre_pkpy_vm, 0);
     }
-    pkpy_pop_top(vm);
+    pkpy_pop_top(cre_pkpy_vm);
 }
 
 void pkpy_sc_on_end(CreEntity entity) {
-    SE_ASSERT(vm);
-    cre_pkpy_entity_instance_cache_push_entity_instance(vm, entity);
-    if (pkpy_getattr(vm, pyEndFunctionName)) {
-        pkpy_push_null(vm);
-        pkpy_vectorcall(vm, 0);
+    SE_ASSERT(cre_pkpy_vm);
+    cre_pkpy_entity_instance_cache_push_entity_instance(cre_pkpy_vm, entity);
+    if (pkpy_getattr(cre_pkpy_vm, pyEndFunctionName)) {
+        pkpy_push_null(cre_pkpy_vm);
+        pkpy_vectorcall(cre_pkpy_vm, 0);
     }
-    pkpy_pop_top(vm);
+    pkpy_pop_top(cre_pkpy_vm);
 }
 
 void pkpy_sc_on_network_callback(const char* message) {
-    SE_ASSERT(vm);
-    cre_pkpy_node_event_manager_broadcast_event_string(vm, CRE_PKPY_NODE_EVENT_OWNER_ID_NETWORK, CRE_PKPY_NODE_EVENT_NAME_MESSAGE_RECEIVED, message);
+    SE_ASSERT(cre_pkpy_vm);
+    cre_pkpy_node_event_manager_broadcast_event_string(cre_pkpy_vm, CRE_PKPY_NODE_EVENT_OWNER_ID_NETWORK, CRE_PKPY_NODE_EVENT_NAME_MESSAGE_RECEIVED, message);
 }
 
 void pkpy_sc_on_script_context_destroy() {
     cre_pkpy_node_event_manager_finalize();
-    cre_pkpy_entity_instance_cache_finalize(vm);
+    cre_pkpy_entity_instance_cache_finalize(cre_pkpy_vm);
 
-    SE_ASSERT(vm);
-    pkpy_delete_vm(vm);
-    vm = NULL;
+    SE_ASSERT(cre_pkpy_vm);
+    pkpy_delete_vm(cre_pkpy_vm);
+    cre_pkpy_vm = NULL;
 
     SE_MEM_FREE(pkpy_script_context);
     pkpy_script_context = NULL;
@@ -202,7 +202,7 @@ void pkpy_sc_on_script_context_destroy() {
 }
 
 pkpy_vm* cre_pkpy_script_context_get_active_pkpy_vm() {
-    return vm;
+    return cre_pkpy_vm;
 }
 
 void cre_pkpy_script_context_on_network_udp_server_client_connected() {}
@@ -212,22 +212,22 @@ void cre_pkpy_script_context_on_network_udp_server_client_connected() {}
 // NODE
 void pkpy_node_event_node_scene_entered(SESubjectNotifyPayload* payload) {
     const CreEntity entity = *(CreEntity*)payload->data;
-    cre_pkpy_node_event_manager_broadcast_event(vm, (int)entity, CRE_PKPY_NODE_EVENT_NAME_SCENE_ENTERED);
+    cre_pkpy_node_event_manager_broadcast_event(cre_pkpy_vm, (int)entity, CRE_PKPY_NODE_EVENT_NAME_SCENE_ENTERED);
 }
 
 void pkpy_node_event_node_scene_exited(SESubjectNotifyPayload* payload) {
     const CreEntity entity = *(CreEntity*)payload->data;
-    cre_pkpy_node_event_manager_broadcast_event(vm, (int)entity, CRE_PKPY_NODE_EVENT_NAME_SCENE_EXITED);
-    cre_pkpy_node_event_manager_remove_entity_and_connections(vm, (int)entity);
+    cre_pkpy_node_event_manager_broadcast_event(cre_pkpy_vm, (int)entity, CRE_PKPY_NODE_EVENT_NAME_SCENE_EXITED);
+    cre_pkpy_node_event_manager_remove_entity_and_connections(cre_pkpy_vm, (int)entity);
 }
 
 // ANIMATED SPRITE
 void pkpy_node_event_animated_sprite_frame_changed(SESubjectNotifyPayload* payload) {
     const AnimatedSpriteFrameChangedPayload* eventPayload = (AnimatedSpriteFrameChangedPayload*)payload->data;
-    cre_pkpy_node_event_manager_broadcast_event_int(vm, (int)eventPayload->entity, CRE_PKPY_NODE_EVENT_NAME_FRAME_CHANGED, eventPayload->newFrame);
+    cre_pkpy_node_event_manager_broadcast_event_int(cre_pkpy_vm, (int)eventPayload->entity, CRE_PKPY_NODE_EVENT_NAME_FRAME_CHANGED, eventPayload->newFrame);
 }
 
 void pkpy_node_event_animated_sprite_animation_finished(SESubjectNotifyPayload* payload) {
     const AnimatedSpriteAnimationFinishedPayload* eventPayload = (AnimatedSpriteAnimationFinishedPayload*)payload->data;
-    cre_pkpy_node_event_manager_broadcast_event_string(vm, (int)eventPayload->entity, CRE_PKPY_NODE_EVENT_NAME_ANIMATION_FINISHED, eventPayload->animation->name);
+    cre_pkpy_node_event_manager_broadcast_event_string(cre_pkpy_vm, (int)eventPayload->entity, CRE_PKPY_NODE_EVENT_NAME_ANIMATION_FINISHED, eventPayload->animation->name);
 }
