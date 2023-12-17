9b121ed95a9d97211fc2adff913b49e2e35bd3a9
chukobyte <chukobyte@gmail.com>
2023-12-16 22:32:17 -0500

    Clean up.

--- Functions.cmake
+++ Functions.cmake
@@ -4,7 +4,7 @@ function(install_seika)
         FetchContent_Declare(
                 seika
                 GIT_REPOSITORY https://github.com/Chukobyte/seika.git
-                GIT_TAG v0.0.10
+                GIT_TAG v0.0.11
         )
         FetchContent_MakeAvailable(seika)
     endif ()
