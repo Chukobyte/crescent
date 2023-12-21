e86b4819e193b4d01a6d3216c962e1c735b8d288
chukobyte <chukobyte@gmail.com>
2023-12-20 22:18:59 -0500

    Updates.

--- engine/src/core/ecs/component/particles2d_component.h
+++ engine/src/core/ecs/component/particles2d_component.h
@@ -6,15 +6,25 @@ extern "C" {
 
 #include <seika/math/se_math.h>
 
+#define CRE_PARTICLES_2D_MAX 100
+
+typedef struct CreParticle2D {
+    SEVector2 position;
+    SEVector2 velocity;
+    SEVector2 acceleration;
+    SEColor color;
+    float timeActive;
+} CreParticle2D;
+
 typedef struct Particles2DComponent {
     // Configuration
-    SEVector2 position;
     SEVector2 linearVelocity;
     SEVector2 linearAcceleration;
     SEColor color;
     float lifeTime;
     float damping;
     struct SETexture* texture;
+    CreParticle2D particles[CRE_PARTICLES_2D_MAX];
 } Particles2DComponent;
 
 Particles2DComponent* particles2d_component_create();
