d8ed738486ddd100b608c5af0a052c96bea6a163
Chukobyte <michaelsmith8293@gmail.com>
2024-01-26 23:49:13 -0500

    Clean.

--- test_games/pkpy_test/src/main.py
+++ test_games/pkpy_test/src/main.py
@@ -8,14 +8,14 @@ class Main(Node2D):
         # text_label.position = Vector2(30, 30)
         # self.add_child(text_label)
 
-        particles2d = Particles2D.new()
-        particles2d.position = Vector2(300, 300)
-        particles2d.initial_velocity = MinMaxVector2(
-            Vector2(-90, -60),
-            Vector2(900, 600)
-        )
+        # particles2d = Particles2D.new()
+        # particles2d.position = Vector2(300, 300)
+        # particles2d.initial_velocity = MinMaxVector2(
+        #     Vector2(-90, -60),
+        #     Vector2(900, 600)
+        # )
         # particles2d.explosiveness = 1.0
-        self.add_child(particles2d)
+        # self.add_child(particles2d)
 
         Engine.set_fps_display_enabled(True)
 
