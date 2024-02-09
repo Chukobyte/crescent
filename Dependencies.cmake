e9646d859f0d219e3e980f3761ec5c9f7af9a790
Chukobyte <michaelsmith8293@gmail.com>
2024-02-09 08:20:06 -0500

    Renamed dependency file with cmake functions.

---
+++ Dependencies.cmake
@@ -0,0 +1,29 @@
+function(install_seika)
+    if (NOT TARGET seika)
+        include(FetchContent)
+        FetchContent_Declare(
+                seika
+                GIT_REPOSITORY https://github.com/Chukobyte/seika.git
+                GIT_TAG v0.0.31
+        )
+        FetchContent_MakeAvailable(seika)
+    endif ()
+endfunction()
+
+function(install_pocketpy)
+    if (NOT TARGET pocketpy)
+        include(FetchContent)
+        option(PK_BUILD_STATIC_LIB "Build static library" ON)
+        FetchContent_Declare(
+                pocketpy_content
+                GIT_REPOSITORY https://github.com/blueloveTH/pocketpy.git
+                GIT_TAG v1.4.0
+        )
+        FetchContent_MakeAvailable(pocketpy_content)
+    endif ()
+    target_include_directories(${PROJECT_NAME} PUBLIC
+            "${PROJECT_BINARY_DIR}"
+            "../include"
+            "${pocketpy_content_SOURCE_DIR}/include"
+    )
+endfunction()
